1b64c52824badb9826fc6cff67b03a829aa9baed
paulozterra <66433825+paulozterra@users.noreply.github.com>
2022-05-24 16:38:05 -0500

    ok

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <memory>
+
 using namespace std;
 
 class singleton{
 
 private:
-  
-  static singleton* s;
-  singleton() = default;
-  
+
+    static singleton* s;
+    singleton() = default;
+
 public:
 
-  static singleton* GetSingleton(){
-    if(s == nullptr ){
-       s = new singleton;
-    }
-    return s;
-  };
-  ~singleton()=default;
+    static singleton* GetSingleton(){
+        if(s == nullptr ){
+            unique_ptr<singleton> s;
+        }
+        return s;
+    };
+    ~singleton()=default;
 };
 
-singleton* singleton::s = nullptr; 
+singleton* singleton::s = nullptr;
 int main(){
-    auto s = singleton::GetSingleton();
+    singleton* s;
+    s->GetSingleton();
     cout<<"singleton creado!!!";
     return 0;
 }

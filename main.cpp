52c59d4dcbba8370e5fab17a0f344a3369597171
paulozterra <66433825+paulozterra@users.noreply.github.com>
2022-05-24 15:56:21 -0500

    nice

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+class singleton{
+
+private:
+  
+  static singleton* s;
+  singleton() = default;
+  
+public:
+
+  static singleton* GetSingleton(){
+    if(s == nullptr ){
+       s = new singleton;
+    }
+    return s;
+  };
+  ~singleton()=default;
+};
+
+singleton* singleton::s = nullptr; 
+int main(){
+    auto s = singleton::GetSingleton();
+    cout<<"singleton creado!!!";
+    return 0;
+}
